538d28489d0f7169a1e12a3be622e7ae673f1e84
KaoriHelen <hki23@inf.ufpr.br>
2024-10-08 02:49:44 -0300

    set and get methods for Property and Space classes

--- src/Property.java
+++ src/Property.java
@@ -2,13 +2,29 @@
  * Property
  */
 public class Property {
-	String name;
-	int values[9];
-	int houses;
+	private String name;
+	private int[] values;
+	private int houses;
     
 	public Property(String name, int values[], int houses) {
 		this.name = name;
 		this.values = values;
 		this.houses = houses;
 	}
+
+        public int getValue(int level) {
+                return this.value[level];
+        }
+
+        public String getName() {
+                return this.name;
+        }
+
+        public int getHouses() {
+                return this.houses;
+        }
+
+        public void setHouses(int houses) {
+                this.houses = houses;
+        }
 }
--- src/Space.java
+++ src/Space.java
@@ -2,11 +2,19 @@
  * Space
  */
 public class Space {
-	String name;
-	int type; //diferenciate luck spaces, property spaces and special spaces
+	private String name;
+	private int type; //diferenciate luck spaces, property spaces and special spaces
     
 	public Space(String name, int type) {
-        this.name = name;
+                this.name = name;
 		this.type = type;
 	}
+
+        public String getName() {
+                return this.name;
+        }
+
+        public int getType() {
+                return this.type;
+        }
 }
