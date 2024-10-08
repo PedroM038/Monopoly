4d966b3bfd37612369274a47ee8ab2871e43cdb8
HELEN KAORI IIJIMA <hki23@inf.ufpr.br>
2024-10-08 01:38:22 -0300

    first classes

--- src/Property.java
+++ src/Property.java
@@ -2,6 +2,13 @@
  * Property
  */
 public class Property {
-
+	String name;
+	int values[9];
+	int houses;
     
+	public Property(String name, int values[], int houses) {
+		this.name = name;
+		this.values = values;
+		this.houses = houses;
+	}
 }
--- src/Space.java
+++ src/Space.java
@@ -2,6 +2,11 @@
  * Space
  */
 public class Space {
-
+	String name;
+	int type; //diferenciate luck spaces, property spaces and special spaces
     
+	public Space(String name, int type) {
+        this.name = name;
+		this.type = type;
+	}
 }
