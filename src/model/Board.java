df74038d0434a40000bd467fbf361cd76da6dc3c
Pedro Marques <pedrohmarques640@gmail.com>
2024-11-12 20:09:25 -0300

    Player and Board

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,6 +1,38 @@
 package model;
 
-public class Board {
+import java.util.ArrayList;
+import java.util.List;
 
-    
+public class Board {
+    private List<Space> spaces;
+
+    public Board() {
+        this.spaces = new ArrayList<>();
+        initializeBoard();
+    }
+
+    /*
+     Método para inicializar os espaços no tabuleiro
+     Ainda falta implementação, pois precisamos definir
+     a ordem dos espaços.
+    */
+    public void initializeBoard() {
+        // Adicione os espaços aqui
+    }
+
+    public Space getSpace(int position) {
+        if (position >= 0 && position < spaces.size()) {
+            return spaces.get(position);
+        } else {
+            throw new IllegalArgumentException("Posição inválida no tabuleiro.");
+        }
+    }
+
+    public int getNumberOfSpaces() {
+        return spaces.size();
+    }
+
+    public void addSpace(Space space) {
+        spaces.add(space);
+    }
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,6 +1,74 @@
 package model;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
 
-    
+public class Player {
+    private String name;
+    private int money;
+    private List<Property> properties;
+    private boolean inJail;
+    private int position;  
+
+    public Player(String name, int initialMoney) {
+        this.name = name;
+        this.money = initialMoney;
+        this.properties = new ArrayList<>();
+        this.inJail = false;
+        this.position = 0; //initial space
+    }
+
+    public boolean buyProperty(Property property) {
+        if (this.money >= property.getActualPrice() && property.buy(this)) {
+            properties.add(property);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sellProperty(Property property) {
+        if (properties.contains(property) && property.sell()) {
+            properties.remove(property);
+            return true;
+        }
+        return false;
+    }
+ 
+    public boolean isInJail() {
+        return inJail;
+    }
+
+    // Método para calcular o valor total das propriedades possuídas
+    public int getTotalAssets() {
+        int totalAssets = this.money;
+        for (Property property : properties) {
+            totalAssets += property.getPrice();
+        }
+        return totalAssets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    // Método para exibir as propriedades possuídas
+    public void showProperties() {
+        System.out.println(name + "'s Properties:");
+        for (Property property : properties) {
+            System.out.println("- " + property.getName());
+        }
+    }
 }
+
