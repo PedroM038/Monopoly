286831f03f8f677f2048b46d2e061fb0fb1016e9
Pedro Marques <pedrohmarques640@gmail.com>
2024-11-20 00:34:11 -0300

    Add dice logic in GUI. Create ControlPanel.

--- Monopoly.java
+++ Monopoly.java
@@ -1,5 +1,8 @@
+import javax.swing.SwingUtilities;
+import view.GameUI;
+
 public class Monopoly {
     public static void main(String[] args) {
-        
+        SwingUtilities.invokeLater(() -> new GameUI());
     }    
 }
--- src/view/GameUI.java
+++ src/view/GameUI.java
@@ -1,6 +1,48 @@
 package view;
 
-public class GameUI {
+import assets.Dice;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.*;
+public class GameUI extends JFrame {
+    private JPanel boardPanel;
+    private JPanel controlPanel;
+    private JButton rollButton;
+    private JLabel diceResultLabel;
+    private final Dice dice = new Dice(6);
 
+    public GameUI() {
+        super("Monopoly");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1080, 720);
+        setLayout(new BorderLayout());
+        
+        initBoardPanel();
+        initControlPanel();
+        
+        setVisible(true);
+    }
+
+    private void initBoardPanel() {
+        boardPanel = new JPanel();
+        add(boardPanel, BorderLayout.CENTER); // Central panel for the board
+    }
     
+    private void initControlPanel() {
+        controlPanel = new JPanel();
+        controlPanel.setLayout(new FlowLayout()); // Layout para organizar botões
+        add(controlPanel, BorderLayout.SOUTH); // Bottom panel for controls
+        
+        rollButton = new JButton("Roll");
+        controlPanel.add(rollButton);
+
+        diceResultLabel = new JLabel("Dice result: ");
+        controlPanel.add(diceResultLabel);
+
+        //adiciona funcionalidade ao botão
+        rollButton.addActionListener(e -> {
+            int result = dice.roll();
+            diceResultLabel.setText("Dice result: " + result);
+        });
+    }
 }
